4effebba4cad0d323e0c3e9f61394fecd2d6a31b
felixChB <44029175+felixChB@users.noreply.github.com>
2019-03-25 20:17:16 +0100

    evenListener

--- Aufgabe0/js/main.js
+++ Aufgabe0/js/main.js
@@ -1,10 +1,14 @@
 //Typescript
-function inputName() {
+function InputName() {
     let userName = prompt("Bitte Namen eingeben", "Name");
     if (userName != null) {
         document.getElementById("you").innerHTML =
             "Jo " + userName + "! Was geht?";
         console.log("Jo " + userName + "! Was geht?");
     }
+}
+document.addEventListener('DOMContentLoaded', init);
+function init() {
+    InputName();
 }
 //# sourceMappingURL=main.js.map
--- Aufgabe0/js/main.ts
+++ Aufgabe0/js/main.ts
@@ -1,10 +1,15 @@
 //Typescript
 
-function inputName() {
+function InputName() {
     let userName: string = prompt("Bitte Namen eingeben", "Name");
     if (userName != null) {
         document.getElementById("you").innerHTML =
             "Jo " + userName + "! Was geht?";
         console.log("Jo " + userName + "! Was geht?")
     }
+}
+document.addEventListener('DOMContentLoaded', init);
+
+function init(): void {
+    InputName()
 }
