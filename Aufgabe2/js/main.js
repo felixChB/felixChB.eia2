ac2a7a9791dfe4f707564bdae41c13b20024a6e0
felixChB <44029175+felixChB@users.noreply.github.com>
2019-04-06 20:01:29 +0200

    mau mau fertig

--- Aufgabe2/js/main.js
+++ Aufgabe2/js/main.js
@@ -1,161 +1,161 @@
 let karo7 = {
     zahl: "7",
-    symbol: "karo",
+    symbol: "Karo",
     img: ""
 };
 let herz7 = {
     zahl: "7",
-    symbol: "herz",
+    symbol: "Herz",
     img: ""
 };
 let pik7 = {
     zahl: "7",
-    symbol: "pik",
+    symbol: "Pik",
     img: ""
 };
 let kreuz7 = {
     zahl: "7",
-    symbol: "kreuz",
+    symbol: "Kreuz",
     img: ""
 };
 let karo8 = {
     zahl: "8",
-    symbol: "karo",
+    symbol: "Karo",
     img: ""
 };
 let herz8 = {
     zahl: "8",
-    symbol: "herz",
+    symbol: "Herz",
     img: ""
 };
 let pik8 = {
     zahl: "8",
-    symbol: "pik",
+    symbol: "Pik",
     img: ""
 };
 let kreuz8 = {
     zahl: "8",
-    symbol: "kreuz",
+    symbol: "Kreuz",
     img: ""
 };
 let karo9 = {
     zahl: "9",
-    symbol: "karo",
+    symbol: "Karo",
     img: ""
 };
 let herz9 = {
     zahl: "9",
-    symbol: "herz",
+    symbol: "Herz",
     img: ""
 };
 let pik9 = {
     zahl: "9",
-    symbol: "pik",
+    symbol: "Pik",
     img: ""
 };
 let kreuz9 = {
     zahl: "9",
-    symbol: "kreuz",
+    symbol: "Kreuz",
     img: ""
 };
 let karo10 = {
     zahl: "10",
-    symbol: "karo",
+    symbol: "Karo",
     img: ""
 };
 let herz10 = {
     zahl: "10",
-    symbol: "herz",
+    symbol: "Herz",
     img: ""
 };
 let pik10 = {
     zahl: "10",
-    symbol: "pik",
+    symbol: "Pik",
     img: ""
 };
 let kreuz10 = {
     zahl: "10",
-    symbol: "kreuz",
+    symbol: "Kreuz",
     img: ""
 };
 let karoBube = {
     zahl: "Bube",
-    symbol: "karo",
+    symbol: "Karo",
     img: ""
 };
 let herzBube = {
     zahl: "Bube",
-    symbol: "herz",
+    symbol: "Herz",
     img: ""
 };
 let pikBube = {
     zahl: "Bube",
-    symbol: "pik",
+    symbol: "Pik",
     img: ""
 };
 let kreuzBube = {
     zahl: "Bube",
-    symbol: "kreuz",
+    symbol: "Kreuz",
     img: ""
 };
 let karoDame = {
     zahl: "Dame",
-    symbol: "karo",
+    symbol: "Karo",
     img: ""
 };
 let herzDame = {
     zahl: "Dame",
-    symbol: "herz",
+    symbol: "Herz",
     img: ""
 };
 let pikDame = {
     zahl: "Dame",
-    symbol: "pik",
+    symbol: "Pik",
     img: ""
 };
 let kreuzDame = {
     zahl: "Dame",
-    symbol: "kreuz",
+    symbol: "Kreuz",
     img: ""
 };
 let karoKoenig = {
     zahl: "Koenig",
-    symbol: "karo",
+    symbol: "Karo",
     img: ""
 };
 let herzKoenig = {
     zahl: "Koenig",
-    symbol: "herz",
+    symbol: "Herz",
     img: ""
 };
 let pikKoenig = {
     zahl: "Koenig",
-    symbol: "pik",
+    symbol: "Pik",
     img: ""
 };
 let kreuzKoenig = {
     zahl: "Koenig",
-    symbol: "kreuz",
+    symbol: "Kreuz",
     img: ""
 };
 let karoAss = {
     zahl: "Ass",
-    symbol: "karo",
+    symbol: "Karo",
     img: ""
 };
 let herzAss = {
     zahl: "Ass",
-    symbol: "herz",
+    symbol: "Herz",
     img: ""
 };
 let pikAss = {
     zahl: "Ass",
-    symbol: "pik",
+    symbol: "Pik",
     img: ""
 };
 let kreuzAss = {
     zahl: "Ass",
-    symbol: "kreuz",
+    symbol: "Kreuz",
     img: ""
 };
 let allCards = [karo7, karo8, karo9, karo10, karoBube, karoDame, karoKoenig, karoAss, herz7, herz8, herz9, herz10, herzBube, herzDame, herzKoenig, herzAss, pik7, pik8, pik9, pik10, pikBube, pikDame, pikKoenig, pikAss, kreuz7, kreuz8, kreuz9, kreuz10, kreuzBube, kreuzDame, kreuzKoenig, kreuzAss];
@@ -164,23 +164,61 @@ let yourHand = [];
 let player1Hand = [];
 let player2Hand = [];
 let player3Hand = [];
+let ablagestapel;
+let randomCount;
+//Aufruf beim Laden der Seite
+document.addEventListener('DOMContentLoaded', init);
+function init() {
+    playGame();
+}
+//ein neues Spiel starten
 function playGame() {
+    //Handkarten auf 0 setzen
+    yourHand = [];
+    document.getElementById("yourHand").innerHTML = "";
     let anfangsHandkarten = parseInt(prompt("Anzahl der Handkarten eingeben", "..."), 10);
+    //ziehStapel auffüllen
     ziehStapel = allCards;
     kartenZiehen(anfangsHandkarten);
+    writeStapel();
     //Test
     console.log(anfangsHandkarten);
 }
+//zufällige Karte ziehen
 function kartenZiehen(anzahlKarten) {
     for (let i = 0; i < anzahlKarten; i++) {
-        let randomCount = Math.floor(Math.random() * ziehStapel.length);
+        randomCount = Math.floor(Math.random() * ziehStapel.length);
         yourHand.push(ziehStapel[randomCount]);
         ziehStapel.splice(randomCount, 1);
         //Test
         console.log(yourHand[i]);
+        writeHtml(yourHand[i]);
     }
     //Test
     console.log(yourHand);
     console.log(ziehStapel);
+}
+//Funktion zum Schreiben des HTML-Codes
+function writeHtml(_handKarte) {
+    /*let card: string = `<div class="Handkarte"><span>${_handKarte.symbol}</span><span>${_handKarte.zahl}</span></div>`;
+    let prodElement = document.createElement('div');
+    prodElement.innerHTML = card;
+    document.getElementById("yourHand").appendChild(prodElement);
+    */
+    //Handkarten als HTML
+    if (_handKarte.symbol == "Karo" || _handKarte.symbol == "Herz") {
+        document.getElementById("yourHand").innerHTML += `<div class="Handkarte red"><p>${_handKarte.symbol}</p><p>${_handKarte.zahl}</p></div>`;
+    }
+    else {
+        document.getElementById("yourHand").innerHTML += `<div class="Handkarte black"><p>${_handKarte.symbol}</p><p>${_handKarte.zahl}</p></div>`;
+    }
+    document.getElementById("HKAnzahl").innerHTML = `<span id="HKAnzahl">Handkarten: ${yourHand.length}</span>`;
+}
+//Ziehstapel und Ablagestapel als HTML
+function writeStapel() {
+    randomCount = Math.floor(Math.random() * ziehStapel.length);
+    ablagestapel = ziehStapel[randomCount];
+    ziehStapel.splice(randomCount, 1);
+    document.getElementById("stapel").innerHTML = `<div id="stapel"><div class="Handkarte"><p>Ablagestapel:</p><p>${ablagestapel.symbol}</p><p>${ablagestapel.zahl}</p></div><div class="Handkarte"><p>Ziehstapel:</p><p>${ziehStapel.length}</p></div></div>`;
 }
 //# sourceMappingURL=main.js.map
--- Aufgabe2/js/main.ts
+++ Aufgabe2/js/main.ts
@@ -1,193 +1,236 @@
-interface Karten{
-    zahl:string;
-    symbol:string;
-    img:string;
+interface Karten {
+    zahl: string;
+    symbol: string;
+    img: string;
 }
 
 let karo7: Karten = {
-    zahl:"7",
-    symbol:"karo",
-    img:""
+    zahl: "7",
+    symbol: "Karo",
+    img: ""
 }
 let herz7: Karten = {
-    zahl:"7",
-    symbol:"herz",
-    img:""
+    zahl: "7",
+    symbol: "Herz",
+    img: ""
 }
 let pik7: Karten = {
-    zahl:"7",
-    symbol:"pik",
-    img:""
+    zahl: "7",
+    symbol: "Pik",
+    img: ""
 }
 let kreuz7: Karten = {
-    zahl:"7",
-    symbol:"kreuz",
-    img:""
+    zahl: "7",
+    symbol: "Kreuz",
+    img: ""
 }
 let karo8: Karten = {
-    zahl:"8",
-    symbol:"karo",
-    img:""
+    zahl: "8",
+    symbol: "Karo",
+    img: ""
 }
 let herz8: Karten = {
-    zahl:"8",
-    symbol:"herz",
-    img:""
+    zahl: "8",
+    symbol: "Herz",
+    img: ""
 }
 let pik8: Karten = {
-    zahl:"8",
-    symbol:"pik",
-    img:""
+    zahl: "8",
+    symbol: "Pik",
+    img: ""
 }
 let kreuz8: Karten = {
-    zahl:"8",
-    symbol:"kreuz",
-    img:""
+    zahl: "8",
+    symbol: "Kreuz",
+    img: ""
 }
 let karo9: Karten = {
-    zahl:"9",
-    symbol:"karo",
-    img:""
+    zahl: "9",
+    symbol: "Karo",
+    img: ""
 }
 let herz9: Karten = {
-    zahl:"9",
-    symbol:"herz",
-    img:""
+    zahl: "9",
+    symbol: "Herz",
+    img: ""
 }
 let pik9: Karten = {
-    zahl:"9",
-    symbol:"pik",
-    img:""
+    zahl: "9",
+    symbol: "Pik",
+    img: ""
 }
 let kreuz9: Karten = {
-    zahl:"9",
-    symbol:"kreuz",
-    img:""
+    zahl: "9",
+    symbol: "Kreuz",
+    img: ""
 }
 let karo10: Karten = {
-    zahl:"10",
-    symbol:"karo",
-    img:""
+    zahl: "10",
+    symbol: "Karo",
+    img: ""
 }
 let herz10: Karten = {
-    zahl:"10",
-    symbol:"herz",
-    img:""
+    zahl: "10",
+    symbol: "Herz",
+    img: ""
 }
 let pik10: Karten = {
-    zahl:"10",
-    symbol:"pik",
-    img:""
+    zahl: "10",
+    symbol: "Pik",
+    img: ""
 }
 let kreuz10: Karten = {
-    zahl:"10",
-    symbol:"kreuz",
-    img:""
+    zahl: "10",
+    symbol: "Kreuz",
+    img: ""
 }
 let karoBube: Karten = {
-    zahl:"Bube",
-    symbol:"karo",
-    img:""
+    zahl: "Bube",
+    symbol: "Karo",
+    img: ""
 }
 let herzBube: Karten = {
-    zahl:"Bube",
-    symbol:"herz",
-    img:""
+    zahl: "Bube",
+    symbol: "Herz",
+    img: ""
 }
 let pikBube: Karten = {
-    zahl:"Bube",
-    symbol:"pik",
-    img:""
+    zahl: "Bube",
+    symbol: "Pik",
+    img: ""
 }
 let kreuzBube: Karten = {
-    zahl:"Bube",
-    symbol:"kreuz",
-    img:""
+    zahl: "Bube",
+    symbol: "Kreuz",
+    img: ""
 }
 let karoDame: Karten = {
-    zahl:"Dame",
-    symbol:"karo",
-    img:""
+    zahl: "Dame",
+    symbol: "Karo",
+    img: ""
 }
 let herzDame: Karten = {
-    zahl:"Dame",
-    symbol:"herz",
-    img:""
+    zahl: "Dame",
+    symbol: "Herz",
+    img: ""
 }
 let pikDame: Karten = {
-    zahl:"Dame",
-    symbol:"pik",
-    img:""
+    zahl: "Dame",
+    symbol: "Pik",
+    img: ""
 }
 let kreuzDame: Karten = {
-    zahl:"Dame",
-    symbol:"kreuz",
-    img:""
+    zahl: "Dame",
+    symbol: "Kreuz",
+    img: ""
 }
 let karoKoenig: Karten = {
-    zahl:"Koenig",
-    symbol:"karo",
-    img:""
+    zahl: "Koenig",
+    symbol: "Karo",
+    img: ""
 }
 let herzKoenig: Karten = {
-    zahl:"Koenig",
-    symbol:"herz",
-    img:""
+    zahl: "Koenig",
+    symbol: "Herz",
+    img: ""
 }
 let pikKoenig: Karten = {
-    zahl:"Koenig",
-    symbol:"pik",
-    img:""
+    zahl: "Koenig",
+    symbol: "Pik",
+    img: ""
 }
 let kreuzKoenig: Karten = {
-    zahl:"Koenig",
-    symbol:"kreuz",
-    img:""
+    zahl: "Koenig",
+    symbol: "Kreuz",
+    img: ""
 }
 let karoAss: Karten = {
-    zahl:"Ass",
-    symbol:"karo",
-    img:""
+    zahl: "Ass",
+    symbol: "Karo",
+    img: ""
 }
 let herzAss: Karten = {
-    zahl:"Ass",
-    symbol:"herz",
-    img:""
+    zahl: "Ass",
+    symbol: "Herz",
+    img: ""
 }
 let pikAss: Karten = {
-    zahl:"Ass",
-    symbol:"pik",
-    img:""
+    zahl: "Ass",
+    symbol: "Pik",
+    img: ""
 }
 let kreuzAss: Karten = {
-    zahl:"Ass",
-    symbol:"kreuz",
-    img:""
+    zahl: "Ass",
+    symbol: "Kreuz",
+    img: ""
 }
 
-let allCards:Karten[] = [karo7,karo8,karo9,karo10,karoBube,karoDame,karoKoenig,karoAss,herz7,herz8,herz9,herz10,herzBube,herzDame,herzKoenig,herzAss,pik7,pik8,pik9,pik10,pikBube,pikDame,pikKoenig,pikAss,kreuz7,kreuz8,kreuz9,kreuz10,kreuzBube,kreuzDame,kreuzKoenig,kreuzAss];
-let ziehStapel:Karten[] = [];
-let yourHand:Karten[] = [];
-let player1Hand:Karten[] = [];
-let player2Hand:Karten[] = [];
-let player3Hand:Karten[] = [];
+let allCards: Karten[] = [karo7, karo8, karo9, karo10, karoBube, karoDame, karoKoenig, karoAss, herz7, herz8, herz9, herz10, herzBube, herzDame, herzKoenig, herzAss, pik7, pik8, pik9, pik10, pikBube, pikDame, pikKoenig, pikAss, kreuz7, kreuz8, kreuz9, kreuz10, kreuzBube, kreuzDame, kreuzKoenig, kreuzAss];
+let ziehStapel: Karten[] = [];
+let yourHand: Karten[] = [];
+let player1Hand: Karten[] = [];
+let player2Hand: Karten[] = [];
+let player3Hand: Karten[] = [];
+let ablagestapel: Karten;
+let randomCount:number;
 
-function playGame(): void{
-    let anfangsHandkarten:number = parseInt(prompt("Anzahl der Handkarten eingeben", "..."), 10);
+//Aufruf beim Laden der Seite
+document.addEventListener('DOMContentLoaded', init);
+
+function init(): void {
+    playGame();
+}
+
+//ein neues Spiel starten
+function playGame(): void {
+    //Handkarten auf 0 setzen
+    yourHand = [];
+    document.getElementById("yourHand").innerHTML = "";
+    let anfangsHandkarten: number = parseInt(prompt("Anzahl der Handkarten eingeben", "..."), 10);
+    //ziehStapel auffüllen
     ziehStapel = allCards;
     kartenZiehen(anfangsHandkarten);
+    writeStapel();
     //Test
     console.log(anfangsHandkarten)
 }
-function kartenZiehen(anzahlKarten:number) :void{
-    for(let i:number=0;i<anzahlKarten;i++){
-        let randomCount:number = Math.floor(Math.random()*ziehStapel.length);
+
+//zufällige Karte ziehen
+function kartenZiehen(anzahlKarten: number): void {
+    for (let i: number = 0; i < anzahlKarten; i++) {
+        randomCount = Math.floor(Math.random() * ziehStapel.length);
         yourHand.push(ziehStapel[randomCount])
         ziehStapel.splice(randomCount, 1)
         //Test
         console.log(yourHand[i])
+        writeHtml(yourHand[i]);
     }
     //Test
     console.log(yourHand)
     console.log(ziehStapel)
+}
+
+//Funktion zum Schreiben des HTML-Codes
+function writeHtml(_handKarte: Karten): void {
+    /*let card: string = `<div class="Handkarte"><span>${_handKarte.symbol}</span><span>${_handKarte.zahl}</span></div>`;
+    let prodElement = document.createElement('div');
+    prodElement.innerHTML = card;
+    document.getElementById("yourHand").appendChild(prodElement);
+    */
+    //Handkarten als HTML
+    if (_handKarte.symbol == "Karo" || _handKarte.symbol == "Herz") {
+        document.getElementById("yourHand").innerHTML += `<div class="Handkarte red"><p>${_handKarte.symbol}</p><p>${_handKarte.zahl}</p></div>`;
+    } else {
+        document.getElementById("yourHand").innerHTML += `<div class="Handkarte black"><p>${_handKarte.symbol}</p><p>${_handKarte.zahl}</p></div>`;
+    }
+    document.getElementById("HKAnzahl").innerHTML = `<span id="HKAnzahl">Handkarten: ${yourHand.length}</span>`
+}
+
+//Ziehstapel und Ablagestapel als HTML
+function writeStapel():void{
+    randomCount = Math.floor(Math.random() * ziehStapel.length);
+    ablagestapel = ziehStapel[randomCount];
+    ziehStapel.splice(randomCount, 1)
+
+    document.getElementById("stapel").innerHTML = `<div id="stapel"><div class="Handkarte"><p>Ablagestapel:</p><p>${ablagestapel.symbol}</p><p>${ablagestapel.zahl}</p></div><div class="Handkarte"><p>Ziehstapel:</p><p>${ziehStapel.length}</p></div></div>`;
 }
