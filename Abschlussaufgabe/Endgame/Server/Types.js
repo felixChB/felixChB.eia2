a22d94850ec42746438b2e7f53437fb171a99c11
felixChB <44029175+felixChB@users.noreply.github.com>
2019-07-27 19:48:58 +0200

    double gelöscht

--- Abschlussaufgabe/Endgame/Server/Types.js
+++
@@ -1,7 +0,0 @@
-/*Abschlussaufgabe: Canvas-Game
-Name: Felix Brunn
-Matrikel: 260550
-Datum: 23.07.2019
-    
-Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
-//# sourceMappingURL=Types.js.map
